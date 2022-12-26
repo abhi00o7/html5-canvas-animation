b76b40a5dd29c2e1653bab8d307ada4d0981c84e
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2022-12-27 03:39:33 +0530

    boilerplate for canvas animation

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d"); // ctx is the context
+const particleArray = []; // array to store all the particles
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+// to view the list of all the properties and methods of the context
 console.log(ctx);
 
 // using addEventListener to resize the canvas when the window is resized
@@ -18,33 +20,59 @@ const mouse = {
 canvas.addEventListener("click", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  console.log(event);
-  drawCircle(mouse.x, mouse.y);
 });
 
 canvas.addEventListener("mousemove", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  console.log(event);
-  drawCircle(mouse.x, mouse.y);
 });
 
-// make drawCircle function
-function drawCircle(x, y) {
-  ctx.fillStyle = "#ff0000";
-  ctx.strokeStyle = "#00f0ff";
-  ctx.lineWidth = 5;
-  ctx.beginPath();
-  // ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-  ctx.arc(x, y, 55, 0, Math.PI * 2);
-  ctx.stroke();
-  ctx.fill();
+// particle class
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    // random number between -0.5 and 0.5
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  draw() {
+    ctx.fillStyle = "#ff0000";
+    ctx.strokeStyle = "#00f0ff";
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    // ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+    ctx.arc(this.x, this.y, 55, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.fill();
+  }
 }
 
-// animate function 
+// create particle array
+function init() {
+  for (let i = 0; i < 100; i++) {
+    particleArray.push(new Particle());
+  }
+}
+init();
+
+function handleParticles() {
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+    particleArray[i].draw();
+  }
+}
+// animate function
 function animate() {
- // clear the canvas before drawing the next frame
+  // clear the canvas before drawing the next frame
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
   // call the drawCircle function
   requestAnimationFrame(animate);
 }
+animate();
